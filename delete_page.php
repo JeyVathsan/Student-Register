c31cedd7a0ce8b630afd6624e83b85b947f891c6
jeyvathsan <ks.jeyvathsan@gmail.com>
2024-08-25 20:47:38 +0530

    first commit

---
+++ delete_page.php
@@ -0,0 +1,22 @@
+<?php include('dbcon.php'); ?>
+
+
+<?php
+
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+
+
+$query="delete from `students` where `ID`= '$id'";
+
+$result=mysqli_query($connection ,$query);
+if(!$result){
+    die("Query failed".mysqli_error());
+}
+else{
+    header('location:index.php?delete_msg=You have deleted the record');
+}
+
+}
+?>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Register</title>
+
+
+    <link rel="stylesheet" href="style.css">
+
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet">
+     
+
+
+</head>
+
+<body>
+    <h1 id="main_title">Students Register(CRUD operation)</h1>
+    <div class="container">
